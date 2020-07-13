b706eae294df60875c9599205de3d3bd21801930
Leroy Grant <lgrant146@gmail.com>
2020-07-13 12:26:49 -0500

    rough draft truncate

--- adlister.sql
+++ adlister.sql
@@ -2,6 +2,8 @@
 CREATE DATABASE if not exists adlister_db;
 USE adlister_db;
 
+DROP TABLE if exists users;
+
 CREATE TABLE if not exists users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     email VARCHAR(100),
@@ -9,6 +11,9 @@ CREATE TABLE if not exists users (
     user_name VARCHAR(100),
     primary key(id)
 );
+
+DROP TABLE if exists ads;
+
 CREATE TABLE if not exists ads (
     title VARCHAR(100),
     description VARCHAR(100),
@@ -16,14 +21,21 @@ CREATE TABLE if not exists ads (
     foreign key (user_id)
                                references users(id)
 );
+
+DROP TABLE if exists categories;
+
 CREATE TABLE if not exists categories (
     name VARCHAR(100),
     description VARCHAR(100),
     id INT UNSIGNED NOT NULL AUTO_INCREMENT
 );
+
+DROP TABLE if exists ad_category;
+
 CREATE TABLE if not exists  ad_category (
     foreign key (ad_id)
                                         references ads(id),
     foreign key (category_id)
                                         references categories(id)
 );
+
