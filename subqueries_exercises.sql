90d595d7cea3d2a90f6f16d3be329dc7b9619547
Leroy Grant <lgrant146@gmail.com>
2020-07-17 09:04:14 -0500

    fixed subqueries

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,1 +1,28 @@
 CREATE DATABASE users;
+# Find all the employees with the same hire date as employee 101010 using a subquery.
+SELECT emp_no
+FROM employees
+WHERE hire_date = (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+);
+
+# Find all the titles held by all employees with the first name Aamod
+SELECT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
+
+# Find all the current department managers that are female.
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > NOW()
+)
+  AND gender = 'F'
